007edfc63ac8db82701bbcc1a34c611c87e42526
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-02-08 17:43:27 +0100

    more refactoring, hero button JS fixed and some minor css fixes

--- JS/index.js
+++ JS/index.js
@@ -60,3 +60,16 @@ function scrollToTop() {
     behavior: "smooth"
   })
 }
+
+
+// // FORM
+
+// const form = document.querySelector(".form");
+
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     const formData = new FormData(form);
+//     const data = Object.fromEntries(formData.entries());
+//     console.log(data);
+    
+// })
