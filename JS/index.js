7e75589dcbdc914fa36b94110c91efec109daf2b
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-02-12 23:43:51 +0100

    form css fix

--- JS/index.js
+++ JS/index.js
@@ -78,13 +78,3 @@ function scrollToTop() {
 
 
 // ACORDION
-
-const accordionItems = document.querySelectorAll('.accordion-item');
-
-accordionItems.forEach(item => {
-  const header = item.querySelector('.accordion-header');
-  header.addEventListener('click', () => {
-    const content = item.querySelector('.accordion-content');
-    content.style.display = content.style.display === 'block' ? 'none' : 'block';
-  });
-});
