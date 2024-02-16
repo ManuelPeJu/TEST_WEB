c870485ad966e62201b3db5c3918abcd22454b13
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-02-16 01:59:17 +0100

    about us css update

--- JS/form.js
+++ JS/form.js
@@ -34,3 +34,33 @@ form.addEventListener("submit", (e) => {
     });
 });
 
+
+
+//NAVBAR SCRIPT
+document.addEventListener('DOMContentLoaded', function () {
+    const mobileMenuToggle = document.getElementById('mobile-menu');
+    const menu = document.getElementById('menu');
+
+    mobileMenuToggle.addEventListener('click', function () {
+        menu.style.display = (menu.style.display === 'flex') ? 'none' : 'flex';
+    });
+
+    window.addEventListener('resize', function () {
+        if (window.innerWidth > 768) {
+            menu.style.display = 'flex';
+        } else {
+            menu.style.display = 'none';
+        }
+    });
+});
+
+
+const navbarEl = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        navbarEl.classList.add("active");
+    } else {
+        navbarEl.classList.remove("active");
+    }
+})
